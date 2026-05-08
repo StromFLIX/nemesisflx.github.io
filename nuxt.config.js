ced9efd6ba03d1b597e3fa2ad58f01fe104908cc
StromFLIX <felix.moenckemeyer@gmail.com>
2026-05-08 19:21:23 +0200

    feat: add umami

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,13 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' }
+      ],
+      script: [
+        {
+          defer: true,
+          src: 'https://umami.stromflix.com/script.js',
+          'data-website-id': 'e9e1b692-8b21-4b3d-b4b3-b0e9c02f6396'
+        }
       ]
     }
   },
